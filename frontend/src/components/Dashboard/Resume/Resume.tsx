397a4718b0446e343a9620a58da1c821297f7c2e
Sergio_Dev <sergiogarciamanzanares@gmail.com>
2025-05-13 20:41:07 +0200

    Creación de la cabecera del Dashboard.

---
+++ frontend/src/components/Dashboard/Resume/Resume.tsx
@@ -0,0 +1,14 @@
+
+function Resume() {
+  return (
+    <section className="flex flex-wrap gap-6">
+      <div className="bg-white dark:bg-gray-800 rounded-xl shadow p-6 flex-1 min-w-[300px] max-w-sm">
+        <p className="text-center text-2xl font-medium text-gray-500 dark:text-gray-400">
+          € 0.00
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default Resume
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,16 +1,45 @@
+// src/pages/Dashboard.tsx
+import { PlusCircleIcon } from "@heroicons/react/24/solid";
 import { useAuth } from "../context/AuthContext";
+import Resume from "../components/Dashboard/Resume/Resume";
 
 export default function Dashboard() {
   const { user } = useAuth();
+  const portfolioName = user ? `Portfolio de ${user.name}` : "Mi Portfolio";
 
   return (
-    <div className="text-center mt-10">
-      <h1 className="text-3xl font-bold text-violet-800 dark:text-white">
-        Bienvenido, {user?.name}
-      </h1>
-      <p className="text-lg mt-4 text-slate-700 dark:text-slate-200">
-        Este es tu panel de control.
-      </p>
+    <div className="w-full pt-15 px-20 bg-gray-50 dark:bg-gray-900 min-h-screen">
+
+      <div className="max-w-4xl ml-6">
+
+        {/* ─── Cabecera ────────────────────────────────────────────── */}
+        <section className="flex items-center justify-start my-8">
+          <h1 className="text-3xl font-bold text-gray-800 dark:text-white">
+            {portfolioName}
+          </h1>
+        </section>
+
+        <div className="flex items-center justify-start gap-4">
+
+          <Resume />
+          <Resume />
+          <Resume />
+        </div>
+
+
+        <div className="mt-8">
+          <button
+            className="flex items-center gap-2 text-amber-500 hover:text-amber-600 dark:text-rose-400 dark:hover:text-rose-300"
+            type="button"
+          >
+            <PlusCircleIcon className="w-8 h-8" />
+            <span className="text-base font-medium">Añade Activo</span>
+          </button>
+        </div>
+
+        {/* ─── Línea separadora abajo ──────────────────────────────── */}
+        <hr className="my-10 border-gray-200 dark:border-gray-700" />
+      </div>
     </div>
   );
 }
